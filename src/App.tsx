a5e0ffbbc893804aa5bddfc191e0e4cb97b40506
Mayowa-de <mayortech03@gmail.com>
2025-10-01 15:14:36 -0700

    added ToggleModeApp file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import {  createContext, useState } from 'react'
 import Counter from './Counter/Counter'
 import './index.css'
+
+interface ThemeContextType{
+  theme : string;
+};
+const ThemeContext = createContext<ThemeContextType | null>(null);
+
 function App() {
+  const [theme, setTheme] = useState('light');
 
   return (
     <div className='flex flex-col justify-center items-center w-full min-h-screen'>
+      <ThemeContext.Provider value = {{theme}}>
       <Counter/>
+      </ThemeContext.Provider>
     </div>
   )
 }
---
+++ src/ToggleModeApp/ToggleModeApp.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ToggleModeApp() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
